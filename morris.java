fbbb78ea18480d4c0515b1e798e9a561cb091438
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-11-30 19:17:51 -0500

    Create morris.java

---
+++ morris.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+            else{
+                TreeNode pre=curr.left;
+                while(pre.right!=null && pre.right!=curr){
+                    pre=pre.right;
+                }
+                if(pre.right==null){
+                    pre.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    ans.add(curr.val);
+                    curr=curr.right;
+                    pre.right=null;   
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
